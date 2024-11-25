e0efa5a3466e325a36fe50b9ecafc8001b79db00
Spacefox95 <nathan.raynal@gmail.com>
2024-11-25 14:15:18 +0100

    ok

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,7 @@
+function welcomeMessage(fullName) {
+	alert(`Welcome ${fullName}`);
+}
+
+const guillaume = welcomeMessage("Guillaume");
+const alex = welcomeMessage("Alex");
+const fred = welcomeMessage('Fred');
